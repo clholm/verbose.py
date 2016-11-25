2a721e54e04f188c555d4522b0d35a476df29626
Connor <clholm@umich.edu>
2016-11-25 02:19:22 -0500

    Function verbose_counter performs most of the work. Need to add error checking and debug more.

--- verbose.py
+++ verbose.py
@@ -1,29 +1,67 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from urllib.error import HTTPError
+from lxml import html
+import requests
 
-# F U N C T I O N S
+# functions
 
-# opens the web page and parses it
-def openSite(url):
-    try: 
-        html = urlopen(url)
-    except ValueError as e: 
-        return None
-    try: 
-        soup = BeautifulSoup(html, 'lxml')
-        text = soup.findAll(text = True) 
-    except AttributeError as e: 
-        return None
-    return text
+# turns a string into individual, all lowercase words and places 
+# them into a dictionary to count them
+def build_dictionary(string): 
+    word_dictionary = dict()
+    j = 0
 
-# receive input
-website = input('Enter a URL ')
+    for i in range (len(string)):
 
-# call openSite function
-text = openSite(website)
+        if (string[i].isalnum()) is False:
+            append_it = string[j:i].lower()
+            if append_it != "": 
 
-if text is None:
-    print("URL could not be found")
-else:
-    print(text)
+                if append_it in word_dictionary: 
+                    word_dictionary[append_it] += 1
+                else: 
+                    word_dictionary[append_it] = 1
+                
+            j = i + 1
+
+    append_it = string[j:].lower()
+
+    if append_it != "": 
+        if append_it in word_dictionary: 
+            word_dictionary[append_it] += 1
+        else: 
+            word_dictionary[append_it] = 1
+
+    return (word_dictionary)
+
+
+def verbose_counter(website, number_of_words): 
+
+    r = requests.get(website)
+    tree = html.fromstring(r.content)
+    tags = tree.xpath('//body')
+
+    if tags: 
+        body = tags[0]
+        text = body.text_content()
+    else: 
+        text = '' 
+
+    word_dictionary = build_dictionary(text)
+
+    repeatedList = []
+
+    # loop through the dictionary 
+    for key, value in word_dictionary.items(): 
+        repeatedList.append([value, key]) 
+
+    repeatedList.sort(reverse=True)
+
+    i = 0
+    frequent_words = [] 
+
+    if repeatedList != []:
+        while i < number_of_words:
+            frequent_words.append(repeatedList[i][1])
+            i += 1 
+
+    return frequent_words
+
