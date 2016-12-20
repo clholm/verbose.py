3050a0a2e9074fedd126dcc69af2a4337b212e2f
Connor <clholm@umich.edu>
2016-12-20 02:40:19 -0500

    Added support for the "duplicates" parameter and added a lambda to the sorting of the repeated_lsit so that it would be in descending frequency and then alphabetic order

--- verbose.py
+++ verbose.py
@@ -15,8 +15,8 @@ def build_dictionary(string):
         if string[i].isalnum(): 
             append_string = append_string + string[i].lower()
         else: 
-            if append_string != "": 
-                
+            if append_string != "":   
+
                 if append_string in word_dictionary: 
                     word_dictionary[append_string] += 1
                 else: 
@@ -28,7 +28,7 @@ def build_dictionary(string):
 
 # driver function that pulls data from the website, calls the build dictionary
 # function, and retrieves the most frequently used words
-def verbose_counter(website, number_of_words): 
+def verbose_counter(website, number_of_words, duplicates_allowed = True): 
 
     # get the website
     r = requests.get(website, timeout = 5)
@@ -50,16 +50,47 @@ def verbose_counter(website, number_of_words):
     for key, value in word_dictionary.items(): 
         repeated_list.append([value, key]) 
 
-    # sort list in descending order of frequency 
-    repeated_list.sort(reverse=True)
+    # sort list in descending order of frequency first 
+    # and then alphabetically
+    repeated_list.sort(key=lambda x: (-x[0], x[1]))
 
     i = 0
     frequent_words = [] 
 
-    if repeated_list != []:
-        while i < number_of_words:
-            frequent_words.append(repeated_list[i][1])
-            i += 1 
+    if repeated_list != []: 
+             
+        if duplicates_allowed: 
+            while i < number_of_words:
+
+                # if this is the last word to append to the list, 
+                # check to see if there are multiple words with the same 
+                # frequency
+                if i < len(repeated_list): 
+                    if i == number_of_words - 1:
+                        # initialize variable j 
+                        j = i
+                        while j < len(repeated_list): 
+                            # if the word has the same frequency, append it to
+                            # frequent_words, otherwise break
+                            if (repeated_list[i][0] == repeated_list[j][0]): 
+                                frequent_words.append(repeated_list[j][1])
+                            else: 
+                                break
+
+                            j += 1
+                    else: 
+                        frequent_words.append(repeated_list[i][1])
+                else: 
+                    break 
+
+                i += 1
+        else: 
+            while i < number_of_words:
+
+                if i < len(repeated_list):
+                    frequent_words.append(repeated_list[i][1])
+
+                i += 1 
 
     return frequent_words
 
