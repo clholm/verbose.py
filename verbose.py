aa663cbce9d8d925b833dd0190474173fcfa7b42
Connor <clholm@umich.edu>
2016-12-21 02:13:34 -0500

    Added support for optional stopword removal

--- verbose.py
+++ verbose.py
@@ -4,9 +4,8 @@ import requests
 # functions
 
 # turns a string into individual, all lowercase words and places 
-# them into a dictionary to count them
-def build_dictionary(string): 
-    word_dictionary = dict()
+# them into a dictionary to count them. Includes stopwords
+def build_dictionary_stopwords(string, word_dictionary): 
     append_string = ""
     j = 0
 
@@ -26,9 +25,64 @@ def build_dictionary(string):
 
     return (word_dictionary)
 
+
+# turns a string into individual, all lowercase words and places 
+# them into a dictionary to count them. Doesn't include stopwords
+def build_dictionary_no_stopwords(string, word_dictionary): 
+    append_string = ""
+    j = 0
+
+    # set that includes every english stopword as defined by NLTK
+    stopwords = set(["i", "me", "my", "myself", "we", "our",
+                    "ours", "ourselves", "you", "your", "yours", 
+                    "yourself", "yourselves", "he", "him", "his", 
+                    "himself", "she", "her", "hers", "herself", 
+                    "it", "its", "itself", "they","them", 
+                    "their", "theirs", "themselves", "what",
+                    "which", "who", "whom", "this", "that", 
+                    "these", "those", "am", "is", "are", 
+                    "was", "were", "be", "been", "being", 
+                    "have", "has", "had", "having", "do", 
+                    "does", "did", "doing", "a", "an", 
+                    "the", "and", "but", "if", "or", 
+                    "because", "as", "until", "while", "of", 
+                    "at", "by", "for", "with", "about", 
+                    "against", "between", "into", "through", "during", 
+                    "before", "after", "above", "below", "to", 
+                    "from", "up", "down", "in", "out", 
+                    "on", "off", "over", "under", "again", 
+                    "further", "then", "once", "here", "there", 
+                    "when", "where", "why", "how", "all", 
+                    "any", "both", "each", "few", "more", 
+                    "most", "other", "some", "such", "no", 
+                    "nor", "not", "only", "own", "same", 
+                    "so", "than", "too", "very", "s", 
+                    "t", "can", "will", "just", 
+                    "don", "should", "now"])
+
+    for i in range (len(string)):
+
+        if string[i].isalnum(): 
+            append_string = append_string + string[i].lower()
+        else: 
+            if append_string != "":   
+                # if append_string isn't a stopword, add it to word_dictionary
+                if append_string not in stopwords: 
+                    if append_string in word_dictionary: 
+                        word_dictionary[append_string] += 1
+                    else: 
+                        word_dictionary[append_string] = 1
+
+                append_string = ""
+
+    return (word_dictionary)
+
+
 # driver function that pulls data from the website, calls the build dictionary
 # function, and retrieves the most frequently used words
-def verbose_counter(website, number_of_words, duplicates_allowed = True): 
+def verbose_counter(website, number_of_words, 
+                    duplicates_allowed = True, 
+                    stopwords_allowed = False): 
 
     # get the website
     r = requests.get(website, timeout = 5)
@@ -41,8 +95,13 @@ def verbose_counter(website, number_of_words, duplicates_allowed = True):
     else: 
         text = '' 
 
-    # call the build_dictionary function
-    word_dictionary = build_dictionary(text)
+    word_dictionary = dict()
+
+    # call one of the build_dictionary functions
+    if stopwords_allowed: 
+        build_dictionary_stopwords(text, word_dictionary)
+    else: 
+        build_dictionary_no_stopwords(text, word_dictionary)
 
     repeated_list = []
 
@@ -92,5 +151,5 @@ def verbose_counter(website, number_of_words, duplicates_allowed = True):
 
                 i += 1 
 
-    return frequent_words
+    return repeated_list
 
